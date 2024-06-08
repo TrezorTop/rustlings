65a09d51293d8089839d692796164b9c16d7c593
Andrey Seluykov <trezortop92@gmail.com>
2024-06-08 15:50:55 +0500

    quiz1 section completion

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples_number: i32) -> i32 {
+    if apples_number > 40 {
+        return apples_number;
+    }
+
+    apples_number * 2
+}
 
 // Don't modify this function!
 #[test]
